1735c86076a222e6388aad701a67c1874c9f4d84
Chandra655 <chandrakanta.khuntia10@gmail.com>
2022-10-15 23:53:25 +0530

    first commit

---
+++ EMEA_GOP/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace EMEA_GOP
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
