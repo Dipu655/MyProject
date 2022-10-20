7de63442dfcca8466172a2e484263f8945744deb
Chandra655 <chandrakanta.khuntia10@gmail.com>
2022-10-20 12:26:36 +0530

    Cal method added

--- EMEA_GOP/UnitTest1.cs
+++ EMEA_GOP/UnitTest1.cs
@@ -14,5 +14,29 @@ namespace EMEA_GOP
             Console.WriteLine("Hi");
             Assert.Pass();
         }
+
+        [Test]
+        public void Cal()
+        {
+            //Arrange
+            //I am on google page
+            Console.WriteLine("Enter a number");
+
+
+
+
+            //I have enter x in search text box
+
+            //I click Search Button
+
+
+            //Act
+
+
+            //Assert
+
+
+
+        }
     }
 }
